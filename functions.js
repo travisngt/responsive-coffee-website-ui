39ea757e180b73a3b4b415202db92d4983274ebf
Tuan <111060416+travisngt@users.noreply.github.com>
2023-05-19 15:05:53 +0700

    :))
    ikn wth im doin

--- functions.js
+++ functions.js
@@ -119,3 +119,95 @@ const calculatePrice3 = (options) => {
 
 // Export the calculatePrice3 function
 module.exports.calculatePrice3 = calculatePrice3;
+
+// Function 4: Calculate Price 4
+const calculatePrice4 = (options) => {
+  const { drink_type, size, sugar, milk, whipped_cream } = options;
+
+  let basePrice = 0;
+
+  if (drink_type === 'coffee') {
+    if (size === 'S') {
+      basePrice = 2.5;
+    } else if (size === 'M') {
+      basePrice = 3.0;
+    } else if (size === 'L') {
+      basePrice = 3.5;
+    }
+  } else if (drink_type === 'tea') {
+    if (size === 'S') {
+      basePrice = 2.0;
+    } else if (size === 'M') {
+      basePrice = 2.5;
+    } else if (size === 'L') {
+      basePrice = 3.0;
+    }
+  }
+
+  let additionalCost = 0;
+
+  if (sugar === 'extra') {
+    additionalCost += 0.5;
+  }
+
+  if (milk === 'extra') {
+    additionalCost += 0.5;
+  }
+
+  if (whipped_cream === 'with') {
+    additionalCost += 1.0;
+  }
+
+  const totalPrice = basePrice + additionalCost;
+
+  return totalPrice;
+};
+
+// Export the calculatePrice4 function
+module.exports.calculatePrice4 = calculatePrice4;
+
+// Function 5: Calculate Price 5
+const calculatePrice5 = (options) => {
+  const { drink_type, size, sugar, milk, whipped_cream } = options;
+
+  let basePrice = 0;
+
+  if (drink_type === 'coffee') {
+    if (size === 'S') {
+      basePrice = 2.5;
+    } else if (size === 'M') {
+      basePrice = 3.0;
+    } else if (size === 'L') {
+      basePrice = 3.5;
+    }
+  } else if (drink_type === 'tea') {
+    if (size === 'S') {
+      basePrice = 2.0;
+    } else if (size === 'M') {
+      basePrice = 2.5;
+    } else if (size === 'L') {
+      basePrice = 3.0;
+    }
+  }
+
+  let additionalCost = 0;
+
+  if (sugar === 'extra') {
+    additionalCost += 0.5;
+  }
+
+  if (milk === 'extra') {
+    additionalCost += 0.5;
+  }
+
+  if (whipped_cream === 'with') {
+    additionalCost += 1.0;
+  }
+
+  const totalPrice = basePrice + additionalCost;
+
+  return totalPrice;
+};
+
+// Export the calculatePrice5 function
+module.exports.calculatePrice5 = calculatePrice5;
---
+++ server.js
@@ -0,0 +1,2 @@
+const { calculatePrice1, calculatePrice2, calculatePrice3, calculatePrice4, calculatePrice5 } = require('./functions');
+
